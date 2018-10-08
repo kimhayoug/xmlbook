3bf8f442cbe106b0d13bfb50ea3656cd3afebe9e
D7703_23 <d7703_23@d7703-23ui-imac.local>
2018-10-08 12:30:31 +0900

    add

---
+++ xmlbook/Tableview.swift
@@ -0,0 +1,26 @@
+//
+//  Tableview.swift
+//  xmlbook
+//
+//  Created by D7703_23 on 2018. 10. 8..
+//  Copyright © 2018년 D7703_23. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
--- xmlbook/ViewController.swift
+++ xmlbook/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, XMLParserDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    
+    @IBOutlet weak var TableView: UITableView!
+    
+    
+    // 딕셔너리의 배열 저장 : item
+    var item:[[String:String]] = []
+    
+    // 딕셔너리 : item [key:value]
+    var elements:[[String:String]] = []
+    
+    var currentElement = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        TableView.dataSource = self
+        
+        if let path = Bundle.main.url(forResource: "book", withExtension: "xml") {
+            
+            if let paser = XMLParser(contentsOf: path) {
+                paser.delegate = self
+                
+                if paser.parse() {
+                    print("파싱 성공")
+                    
+                    print(elements)
+                    
+                } else {
+                    print("파싱 오류2")
+                }
+            } else {
+                print("파싱 오류1")
+            }
+        } else {
+            print("XML 파일 없음")
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        
+        let data = string.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+        
+        if !data.isEmpty {
+            elements[currentElement] = data
+        }
+        
 }
-
+}
